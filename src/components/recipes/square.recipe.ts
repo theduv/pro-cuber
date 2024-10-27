6a52f78083e323bf50f6ef2228dc9bb43c4e5bea
Théo <tduval@student.42.fr>
2024-10-27 23:24:17 +0100

    feat: brighter colors (kinda)

--- src/components/recipes/square.recipe.ts
+++ src/components/recipes/square.recipe.ts
@@ -10,8 +10,8 @@ export const squareRecipe = cva({
       W: { backgroundColor: 'neutral.50' },
       R: { backgroundColor: 'error.500' },
       B: { backgroundColor: 'info.500' },
-      Y: { backgroundColor: 'warning.400' },
-      O: { backgroundColor: 'orange.400' },
+      Y: { backgroundColor: 'warning.300' },
+      O: { backgroundColor: 'orange.500' },
       G: { backgroundColor: 'success.500' },
     },
     size: {
